b7f6a6a879f4e2f1fb993f4a6e331d1b8d07b4a5
luis <luisjimenezpalacios83@gmail.com>
2024-12-08 04:59:44 -0400

    update

--- bin/player/player.gd
+++ bin/player/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+
+
 @onready var sprite = $Col/Imgs
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
---
+++ scripts/audio.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+@onready var audio_player_1 = $AudioStreamPlayer
+@onready var audio_player_2 = $AudioStreamPlayer2
+
+
+var buffer_time = 0.1 
+
+
+func _ready():
+	audio_player_1.connect("finished", Callable(self, "_on_audio_player_1_finished"))
+	audio_player_2.connect("finished", Callable(self, "_on_audio_player_2_finished"))
+	audio_player_1.play()
+
+
+func _process(_delta):
+	if audio_player_1.is_playing() and audio_player_1.get_playback_position() >= audio_player_1.stream.get_length() - buffer_time:
+		if not audio_player_2.is_playing():
+			audio_player_2.play()
+	if audio_player_2.is_playing() and audio_player_2.get_playback_position() >= audio_player_2.stream.get_length() - buffer_time:
+		if not audio_player_1.is_playing():
+			audio_player_1.play()
+
+
+func _on_audio_player_1_finished():
+	audio_player_2.play()
+
+
+func _on_audio_player_2_finished():
+	audio_player_1.play()
