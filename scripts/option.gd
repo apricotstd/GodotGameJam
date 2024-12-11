c4b0dfaefe08956f9327cf351974c034fe6a2c61
luis <luisjimenezpalacios83@gmail.com>
2024-12-11 13:18:20 -0400

    update

--- scripts/option.gd
+++ scripts/option.gd
@@ -1,33 +1,45 @@
 extends Control
+
 var back = "res://bin/views/menu/menu.tscn"
 var op1 = "res://bin/views/option/option1.tscn"
 var op2 = "res://bin/views/option/option2.tscn"
 var op3 = "res://bin/views/option/option3.tscn"
-# Called when the node enters the scene tree for the first time.
+var volumenM = 1 
+# Llamado cuando el nodo entra en el árbol de la escena por primera vez.
 func _ready():
-	pass # Replace with function body.
+	pass  # Reemplaza con el cuerpo de la función.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Llamado en cada cuadro. 'delta' es el tiempo transcurrido desde el cuadro anterior.
 func _process(delta):
 	pass
 
-
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file(back)
-	pass # Replace with function body.
-
 
 func _on_texture_button_1_pressed():
 	get_tree().change_scene_to_file(op1)
-	pass # Replace with function body.
-
 
 func _on_texture_button_2_pressed():
 	get_tree().change_scene_to_file(op2)
-	pass # Replace with function body.
-
 
 func _on_texture_button_3_pressed():
 	get_tree().change_scene_to_file(op3)
-	pass # Replace with function body.
+
+func _on_control_musica_value_changed(value: float) -> void:
+	volumenM = value
+	print("VolumenM actualizado a: ", volumenM)  
+	volumenMusica(0, value)
+
+func volumenMusica(bus_index, value: float) -> void:
+	value = clamp(value, 0, 100)
+	actualizar_label(value)
+	if value == 0:
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
+		AudioServer.set_bus_volume_db(bus_index, value)
+
+# Función para actualizar el texto del Label
+func actualizar_label(value: float) -> void:
+	$TextureRect/SoundControl/TextureButtonMusica/TextureButton6/Label.text = str(value)
+	print("Label actualizado a: ", str(value))  #
