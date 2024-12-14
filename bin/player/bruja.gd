55e0a61fe33ff935c1ce47c7972aed09cc65db41
shiro <dbeltran486@gmail.com>
2024-12-14 15:56:08 -0400

    mm

--- bin/player/bruja.gd
+++ bin/player/bruja.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 200.0
+@export var speed = 300.0
 var eje = "x"
 
 var mensajesBruja = [
@@ -19,15 +19,11 @@ func changeEje(ej):
 
 func _process(delta: float) -> void:
 	if Gl.player != null:
-		var direction = (Gl.player.global_position - position).normalized()
+		var direction = (Gl.player.global_position - global_position).normalized()
 		velocity = direction * speed
+		velocity.y = 0
 		move_and_slide()
 	pass
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		body.dead()
-	pass # Replace with function body.
 
 func _on_visible_screen_entered() -> void:
 	$Label.text = mensajesBruja[randi_range(0, mensajesBruja.size() - 1) ]
@@ -36,4 +32,9 @@ func _on_visible_screen_entered() -> void:
 
 func _on_visible_screen_exited() -> void:
 	$Label.hide()
+	pass # Replace with function body.
+
+func _on_contact_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.dead()
 	pass # Replace with function body.
